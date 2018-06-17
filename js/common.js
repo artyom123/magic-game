3774c26b44febc548770dbe58f01b64383201b5e
artyom123 <artvirsh@mail.ru>
2018-06-17 15:01:14 +0300

    create landing page

---
+++ js/common.js
@@ -0,0 +1,23 @@
+const navigation = document.querySelector('#navigation');
+const button = document.querySelector('#navbar-toggle');
+
+button.addEventListener('click', openMenu);
+
+function openMenu() {
+    const active = document.querySelector('.active');
+
+    if (active) {
+        navigation.classList.remove('active');
+    } else navigation.classList.add('active');
+}
+
+$(document).ready(function(){
+    $("a[href*=#]").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
